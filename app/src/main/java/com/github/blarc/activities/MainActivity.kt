dd787537aa863b15fa2732e309b69243c7b27d1e
Martin <mb0484@student.uni-ljk.si>
2023-04-01 14:16:01 +0200

    added basic user login

--- app/src/main/java/com/github/blarc/activities/MainActivity.kt
+++ app/src/main/java/com/github/blarc/activities/MainActivity.kt
@@ -1,12 +1,20 @@
 package com.github.blarc.activities
 
+import android.content.ContentValues.TAG
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.github.blarc.MyApplication
 import com.github.blarc.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val curUserId = MyApplication.curUserId
+
+        Log.d(TAG, "Welcome user $curUserId")
+
     }
 }
