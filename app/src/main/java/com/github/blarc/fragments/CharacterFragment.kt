80706ceece2672fb34f776e8fd467b55b2c7d4b5
Martin <mb0484@student.uni-ljk.si>
2023-04-01 22:57:01 +0200

    added on click listeners

--- app/src/main/java/com/github/blarc/fragments/CharacterFragment.kt
+++ app/src/main/java/com/github/blarc/fragments/CharacterFragment.kt
@@ -25,9 +25,42 @@ class CharacterFragment : Fragment() {
     ): View? {
 
 
-        return inflater.inflate(R.layout.fragment_character, container, false)
-    }
+//        return inflater.inflate(R.layout.fragment_character, container, false)
 
+        val view = inflater.inflate(R.layout.fragment_character, container, false)
+
+        val svgImageView1 = view.findViewById<SVGImageView>(R.id.svgImageView1)
+        svgImageView1.setOnClickListener {
+            val item = "head"
+        }
+
+        val svgImageView2 = view.findViewById<SVGImageView>(R.id.svgImageView2)
+        svgImageView2.setOnClickListener {
+            val item = "mainHand"
+        }
+
+        val svgImageView3 = view.findViewById<SVGImageView>(R.id.svgImageView3)
+        svgImageView3.setOnClickListener {
+            val item = "offHand"
+        }
+
+        val svgImageView4 = view.findViewById<SVGImageView>(R.id.svgImageView4)
+        svgImageView4.setOnClickListener {
+            val item = "torso"
+        }
+
+        val svgImageView5 = view.findViewById<SVGImageView>(R.id.svgImageView5)
+        svgImageView5.setOnClickListener {
+            val item = "legs"
+        }
+
+        val svgImageView6 = view.findViewById<SVGImageView>(R.id.svgImageView6)
+        svgImageView6.setOnClickListener {
+            val item = "feet"
+        }
+
+        return view
+    }
 
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
