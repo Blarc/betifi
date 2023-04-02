ec1d8075497f37e6e538b7890ef90eab3b972b16
Blarc <jakob.malezic@gmail.com>
2023-04-02 10:10:01 +0200

    [feat] Items on challenge detail and complete.

--- app/src/main/java/com/github/blarc/fragments/ChallengeCompleteFragment.kt
+++ app/src/main/java/com/github/blarc/fragments/ChallengeCompleteFragment.kt
@@ -2,22 +2,26 @@ package com.github.blarc.fragments
 
 import android.content.Intent
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
-import androidx.fragment.app.activityViewModels
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.fragment.app.Fragment
 import com.github.blarc.R
 import com.github.blarc.activities.MainActivity
+import com.github.blarc.entity.Item
 
 
-class ChallengeCompleteFragment : Fragment() {
+class ChallengeCompleteFragment(private val item: Item?) : Fragment() {
 
     private lateinit var completeChallengeBtn: Button;
+    private lateinit var completeChallengeItemImage: ImageView
+    private lateinit var completeChallengeTextView: TextView
 
     companion object {
-        fun newInstance() = ChallengeCompleteFragment()
+        fun newInstance(item: Item?) = ChallengeCompleteFragment(item)
     }
 
     override fun onCreateView(
@@ -30,6 +34,20 @@ class ChallengeCompleteFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         completeChallengeBtn = view.findViewById(R.id.fragmentChallengeCompleteButtonComplete)
+        completeChallengeItemImage = view.findViewById(R.id.fragmentChallengeCompleteImageView)
+        completeChallengeTextView = view.findViewById(R.id.fragmentChallengeCompleteTextView)
+
+        item?.let {
+
+            completeChallengeTextView.text = item.name
+
+            view.context.resources.getIdentifier(
+                "inventory_item_${it.iconRef}",
+                "drawable",
+                view.context.packageName
+            ).let { imageId -> completeChallengeItemImage.setImageResource(imageId) }
+        }
+
 
         completeChallengeBtn.setOnClickListener {
             val intent = Intent(context, MainActivity::class.java)
--- app/src/main/java/com/github/blarc/fragments/ChallengeDetailFragment.kt
+++ app/src/main/java/com/github/blarc/fragments/ChallengeDetailFragment.kt
@@ -1,15 +1,21 @@
 package com.github.blarc.fragments
 
+import android.opengl.Visibility
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.ImageView
+import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import com.github.blarc.R
 import com.github.blarc.UIUtils.replaceFragment
 import com.github.blarc.entity.Challenge
+import com.github.blarc.entity.Item
+import com.github.blarc.firebase.FirebaseUtils
 
 
 const val ARG_CHALLENGE_DETAILS = "challenge"
@@ -21,7 +27,15 @@ class ChallengeDetailFragment : Fragment() {
     private lateinit var titleTextView: TextView
     private lateinit var durationTextView: TextView
 
-    private var challenge: Challenge? = null
+    private lateinit var requestedItemLayout: LinearLayout
+    private lateinit var bettedItemLayout: LinearLayout
+    private lateinit var acceptDeclineLayout: LinearLayout
+
+    private lateinit var requestedItemImage: ImageView
+
+    private lateinit var bettedItemImage: ImageView
+
+    private lateinit var challenge: Challenge
 
 
     companion object {
@@ -52,24 +66,77 @@ class ChallengeDetailFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        requestedItemLayout = view.findViewById(R.id.challenge_detail_requested_item_layout)
+        bettedItemLayout = view.findViewById(R.id.challenge_detail_betted_item_layout)
+        acceptDeclineLayout = view.findViewById(R.id.details_challenge_accept_decline_layout)
+
+        requestedItemImage = view.findViewById(R.id.challenge_detail_requested_item_icon)
+        bettedItemImage = view.findViewById(R.id.challenge_detail_betted_item_icon)
+
         dueOrCompletedTextView = view.findViewById(R.id.challenge_title_date)
         challengeFinishButton = view.findViewById(R.id.details_challenge_finish_btn)
         titleTextView = view.findViewById(R.id.challenge_title_input)
         descriptionTextView = view.findViewById(R.id.challenge_title_desc)
         durationTextView = view.findViewById(R.id.challenge_detail_duration)
 
-        titleTextView.text = challenge?.name ?: "No title."
-        descriptionTextView.text = challenge?.description ?: "No description"
-        if (challenge?.duration != null) {
-            durationTextView.text = challenge?.duration.toString()
+        var item: Item? = null
+
+        if (challenge.type == "friend") {
+            requestedItemLayout.visibility = View.VISIBLE
+            bettedItemLayout.visibility = View.VISIBLE
+            acceptDeclineLayout.visibility = View.VISIBLE
+            challengeFinishButton.visibility = View.GONE
+
+            challenge.acceptingItem?.let { Log.w("", it.iconRef) }
+            challenge.acceptingItem?.let {
+                view.context.resources.getIdentifier(
+                    "inventory_item_${it.iconRef}",
+                    "drawable",
+                    view.context.packageName
+                ).let { imageId -> requestedItemImage.setImageResource(imageId) }
+            }
+
+            challenge.givingItem?.let { Log.w("", it.iconRef) }
+            challenge.givingItem?.let {
+                view.context.resources.getIdentifier(
+                    "inventory_item_${it.iconRef}",
+                    "drawable",
+                    view.context.packageName
+                ).let { imageId -> bettedItemImage.setImageResource(imageId) }
+            }
+
+        } else {
+            requestedItemLayout.visibility = View.GONE
+            bettedItemLayout.visibility = View.GONE
+            acceptDeclineLayout.visibility = View.GONE
+            challengeFinishButton.visibility = View.VISIBLE
+        }
+
+        titleTextView.text = challenge.name
+        descriptionTextView.text = challenge.description
+        if (challenge.duration != null) {
+            durationTextView.text = challenge.duration.toString()
         }
 
         challengeFinishButton.setOnClickListener {
-            replaceFragment(
-                requireActivity(),
-                R.id.activityChallengeDetailFrameLayout,
-                ChallengeCompleteFragment::class.java
-            )
+
+            if (item == null) {
+                FirebaseUtils.subscribeToItemsOnFirebase {
+                    item = it.random()
+                    replaceFragment(
+                        requireActivity(),
+                        R.id.activityChallengeDetailFrameLayout,
+                        ChallengeCompleteFragment.newInstance(item)
+                    )
+                }
+            }
+            else {
+                replaceFragment(
+                    requireActivity(),
+                    R.id.activityChallengeDetailFrameLayout,
+                    ChallengeCompleteFragment.newInstance(item)
+                )
+            }
         }
     }
 }
